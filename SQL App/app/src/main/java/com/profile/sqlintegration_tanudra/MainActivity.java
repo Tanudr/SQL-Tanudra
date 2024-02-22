77ed105615914078556e51c6b2d5dcfbcadabb0c
Jam Tanudra <158396762+Tanudr@users.noreply.github.com>
2024-02-22 11:45:05 +0800

    Add files via upload

---
+++ SQL App/app/src/main/java/com/profile/sqlintegration_tanudra/MainActivity.java
@@ -0,0 +1,14 @@
+package com.profile.sqlintegration_tanudra;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
